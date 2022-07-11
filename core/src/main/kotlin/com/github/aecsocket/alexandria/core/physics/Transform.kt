67852db5b86ad3373930433a55da441856c1017d
aecsocket <aecsocket@tutanota.com>
2022-07-11 21:41:44 +0100

    Collection diff util, transform tweak

--- core/src/main/kotlin/com/github/aecsocket/alexandria/core/physics/Transform.kt
+++ core/src/main/kotlin/com/github/aecsocket/alexandria/core/physics/Transform.kt
@@ -15,14 +15,14 @@ data class Transform(
 
     // assuming this is an object -> world space transform...
     // · v is an object-space vector
-    fun apply(v: Vector3 = Vector3.Zero) = rot * v + tl
+    fun apply(v: Vector3) = rot * v + tl
 
     // · r is an object-space ray
     fun apply(r: Ray) = if (rot == Quaternion.Identity) Ray(apply(r.pos), r.dir, r.invDir)
         else Ray(apply(r.pos), rot * r.dir)
 
     // · v is a world-space vector
-    fun invert(v: Vector3 = Vector3.Zero) = invRot * (v - tl)
+    fun invert(v: Vector3) = invRot * (v - tl)
 
     // · r is a world-space ray
     fun invert(r: Ray) = if (rot == Quaternion.Identity) Ray(invert(r.pos), r.dir, r.invDir)
---
+++ core/src/test/kotlin/com/github/aecsocket/alexandria/core/CollectionDiffTest.kt
@@ -0,0 +1,53 @@
+package com.github.aecsocket.alexandria.core
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CollectionDiffTest {
+    @Test
+    fun testIdentical() {
+        assertEquals(CollectionDiff(
+            emptySet(), emptySet()
+        ), diff(
+            emptySet<Int>(),
+            emptySet()
+        ))
+
+        assertEquals(CollectionDiff(
+            emptySet(), emptySet()
+        ), diff(
+            setOf(1, 2, 3),
+            setOf(1, 2, 3)
+        ))
+    }
+
+    @Test
+    fun testAdded() {
+        assertEquals(CollectionDiff(
+            setOf(3, 4), emptySet()
+        ), diff(
+            setOf(1, 2),
+            setOf(1, 2, 3, 4)
+        ))
+    }
+
+    @Test
+    fun testRemoved() {
+        assertEquals(CollectionDiff(
+            emptySet(), setOf(3, 4)
+        ), diff(
+            setOf(1, 2, 3, 4),
+            setOf(1, 2)
+        ))
+    }
+
+    @Test
+    fun testAddedAndRemoved() {
+        assertEquals(CollectionDiff(
+            setOf(20, 21), setOf(10, 11)
+        ), diff(
+            setOf(1, 2, 3, 10, 11),
+            setOf(1, 2, 3, 20, 21)
+        ))
+    }
+}
